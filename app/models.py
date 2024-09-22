6bb73a81e220f454934ea74e5dbcd0633f038c64
TheSaviourEking <saviour.eking@outlook.com>
2024-09-22 18:34:10 +0100

    chore: implement The MenuItemType model

--- app/models.py
+++ app/models.py
@@ -21,3 +21,40 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+    __tablename__ = "menus"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    items = db.relationship(
+        "MenuItem", back_populates="menu", cascade="all, delete-orphan"
+    )
+
+
+class MenuItem(db.Model):
+    __tablename__ = "menu_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(
+        db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False
+    )
+
+    type = db.relationship("MenuItemType", back_populates="menu_item")
+    menu = db.relationship("Menu", back_populates="items")
+
+
+class MenuItemType(db.Model):
+    __tablename__ = "menu_item_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    menu_item = db.relationship(
+        "MenuItem", back_populates="type", cascade="all, delete-orphan"
+    )
