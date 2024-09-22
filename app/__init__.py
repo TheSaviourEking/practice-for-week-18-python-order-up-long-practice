38aabd7939c6df34bc64cd5ae6256cdaf20bec71
TheSaviourEking <saviour.eking@outlook.com>
2024-09-22 10:55:43 +0100

    chore: implement Making the login view

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,10 @@ app.register_blueprint(session.bp)
 db.init_app(app)  # Configure the application with SQLAlchemy
 
 login = LoginManager(app)
-login.login_view = "session.login"
+login.login_view = "session.login"  # login manager use the session.login bp function
 
 
+# login manager uses load_user function to get employees for the db
 @login.user_loader
 def load_user(id):
     return Employee.query.get(int(id))
