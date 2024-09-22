ec4390281572f932bea48f02994496629002b3ea
TheSaviourEking <saviour.eking@outlook.com>
2024-09-22 10:41:55 +0100

    chore: implement Making the login view

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from .config import Configuration
-from .routes import orders
+from .routes import orders, sessions
 from .models import db, Employee
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(sessions.bp)
 db.init_app(app)  # Configure the application with SQLAlchemy
 
 login = LoginManager(app)
--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class Employee(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     employee_number = db.Column(db.Integer, nullable=False, unique=True)
-    hashed_password = db.column(db.String(255), nullable=False)
+    hashed_password = db.Column(db.String(255), nullable=False)
 
     @property
     def password(self):
