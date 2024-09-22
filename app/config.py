259defadf21b85885d0af3b0d709cb11bb475f59
TheSaviourEking <saviour.eking@outlook.com>
2024-09-22 09:49:55 +0100

    chore: implement Adding SQLAlchemy

--- app/config.py
+++ app/config.py
@@ -2,4 +2,6 @@ import os
 
 
 class Configuration(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
