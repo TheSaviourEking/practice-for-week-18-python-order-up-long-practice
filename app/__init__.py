fab1775cec7b1e6758fea371e8c81b90560352c4
TheSaviourEking <saviour.eking@outlook.com>
2024-09-22 10:53:12 +0100

    chore: implement Making the login view

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import Configuration
-from .routes import orders, sessions
+from .routes import orders, session
 from .models import db, Employee
 from flask_login import LoginManager, login_user, current_user
 
@@ -8,7 +8,7 @@ from flask_login import LoginManager, login_user, current_user
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
-app.register_blueprint(sessions.bp)
+app.register_blueprint(session.bp)
 db.init_app(app)  # Configure the application with SQLAlchemy
 
 login = LoginManager(app)
